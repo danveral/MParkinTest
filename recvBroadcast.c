04662529eeb9356254a3f68385737da2f5ac0496
lingguang <15654181@qq.com>
2019-05-29 04:01:38 +0000

    20190529 Morning...

--- recvBroadcast.c
+++ recvBroadcast.c
@@ -1,6 +1,15 @@
 // packets loss test according to Mike Parkin's request
 // Author: Guang Ling
 // Version: v1.0
+////////////////////////////////////////////////////
+//
+//     IMPORTANT !!!
+//
+// This Test is for some VLS-128 sensors that under suspicion of packet loss, 
+// NOT for sensors had obvious problems.
+// use veloview or other tools to check the sensor first.
+//
+////////////////////////////////////////////////////
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -16,6 +25,8 @@
 #include <time.h>
 
 #define TEST_HOUR 5  // Input how many hours do you want to test.
+
+int sniff_from_udp(int sockListen, struct sockaddr_in recvAddr);
 
 int main() {
 
@@ -42,28 +53,57 @@ int main() {
         printf("bind fail\n");
         return -1;
     }
-
-    int recvbytes;
-    char recvbuf[1206];
-    // Caution: if Linux, you may use "int" to declare addrLen, that depends on your computer
-    socklen_t addrLen = sizeof(struct sockaddr_in);
     
-    int azimuth_for_first_block;
-    // the vls-128 output 21701388 packets / hour
-    for (int i=0; i<21701389; i++) {
-        if((recvbytes = recvfrom(sockListen, recvbuf, 1206, 0,
-            (struct sockaddr *)&recvAddr, &addrLen)) != -1) {
-            //recvbuf[recvbytes] = '\0';
-            //printf("receive a broadCast messgse:%s\n", recvbuf);
-            azimuth_for_first_block = recvbuf[3]*256 + recvbuf[2];            
-            printf("%d\n",azimuth_for_first_block);
-        } else {
-            printf("recvfrom fail\n");
-        }
-    }
-
+    sniff_from_udp(sockListen, recvAddr);
     close(sockListen);
 
     return 0;
 }
 
+
+int sniff_from_udp(int sockListen, struct sockaddr_in recvAddr) {
+   
+    int recvbytes;
+    char recvbuf[1206];
+    // Caution: if Linux, use "int" to declare addrLen, that depends on your computer
+    // in Mac or some other device, you may use "socklen_t addrLen"
+    int addrLen = sizeof(struct sockaddr_in);
+    
+    int azimuth_for_first_block;
+    // the vls-128 outputs around 21701388 packets / hour
+    // so 20,000,000 is nearly one hour
+    long count = 2000;
+
+    long sensor_time_start;
+    long sensor_time_end;
+    long sensor_time;
+    long time_gap;
+
+    for (int hours=0; hours < TEST_HOUR; hours++){
+        for (long i=0; i<count; i++) {        
+            recvbytes = recvfrom(sockListen, recvbuf, 1206, 0, (struct sockaddr *)&recvAddr, &addrLen);
+            if(recvbytes != -1) {
+                sensor_time = recvbuf[1203]*256*256*256 + recvbuf[1202]*256*256 + recvbuf[1201]*256 + recvbuf[1200];   
+                azimuth_for_first_block = recvbuf[3]*256 + recvbuf[2];            
+            } else {
+                printf("recvfrom fail\n");
+            }
+
+            if (i==0) {
+                sensor_time_start = recvbuf[1203]*256*256*256 + recvbuf[1202]*256*256 + recvbuf[1201]*256 + recvbuf[1200];   
+            }
+
+            if (i==(count-1)) {
+                sensor_time_end = recvbuf[1203]*256*256*256 + recvbuf[1202]*256*256 + recvbuf[1201]*256 + recvbuf[1200];
+            }
+        }
+
+        time_gap = sensor_time_end - sensor_time_start;
+        if (time_gap < 0) {
+            time_gap = time_gap + 3600000000;
+        }
+        printf("%ld\n", time_gap);
+    }
+
+    return 0;
+}
